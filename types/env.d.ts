a54cb7707b5e8bd3ba3469655da7b1bb066c5756
rferrari <495887+rferrari@users.noreply.github.com>
2026-03-14 09:58:46 -0300

    moved the API URLs to environment variables! 🚀

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,4 +1,6 @@
 declare module '@env' {
+  export const API_BASE_URL: string;
+  export const LEADERBOARD_API_URL: string;
   export const LOADING_EFFECT: string;
   export const SNAPS_CONTAINER_AUTHOR: string;
   export const SNAPS_PAGE_MIN_SIZE: string;
